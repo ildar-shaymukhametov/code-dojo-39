5aa25cb9d6c4ef94eed4d24ddf0b829360b9df5d
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-07 21:40:13 +0500

    add support for custom animals

--- csharp/dotnetcore/song/Program.cs
+++ csharp/dotnetcore/song/Program.cs
@@ -11,47 +11,47 @@ namespace song
             Console.WriteLine(song);
         }
 
-        public static string GetSong()
+        public static string GetSong(string animal1 = "fly", string animal2 = "spider", string animal3 = "bird", string animal4 = "cat", string animal5 = "dog", string animal6 = "cow", string animal7 = "horse")
         {
-            return @"There was an old lady who swallowed a fly.
-I don't know why she swallowed a fly - perhaps she'll die!
+            return $@"There was an old lady who swallowed a {animal1}.
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a spider;
+There was an old lady who swallowed a {animal2};
 That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+She swallowed the {animal2} to catch the {animal1};
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a bird;
-How absurd to swallow a bird.
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a {animal3};
+How absurd to swallow a {animal3}.
+She swallowed the {animal3} to catch the {animal2},
+She swallowed the {animal2} to catch the {animal1};
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a cat;
-Fancy that to swallow a cat!
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a {animal4};
+Fancy that to swallow a {animal4}!
+She swallowed the {animal4} to catch the {animal3},
+She swallowed the {animal3} to catch the {animal2},
+She swallowed the {animal2} to catch the {animal1};
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a dog;
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a {animal5};
+What a hog, to swallow a {animal5}!
+She swallowed the {animal5} to catch the {animal4},
+She swallowed the {animal4} to catch the {animal3},
+She swallowed the {animal3} to catch the {animal2},
+She swallowed the {animal2} to catch the {animal1};
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a cow;
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+There was an old lady who swallowed a {animal6};
+I don't know how she swallowed a {animal6}!
+She swallowed the {animal6} to catch the {animal5},
+She swallowed the {animal5} to catch the {animal4},
+She swallowed the {animal4} to catch the {animal3},
+She swallowed the {animal3} to catch the {animal2},
+She swallowed the {animal2} to catch the {animal1};
+I don't know why she swallowed a {animal1} - perhaps she'll die!
 
-There was an old lady who swallowed a horse...
+There was an old lady who swallowed a {animal7}...
 ...She's dead, of course!";
         }
     }
--- csharp/dotnetcore/test/Test.cs
+++ csharp/dotnetcore/test/Test.cs
@@ -7,6 +7,53 @@ namespace test
     public class Test
     {
         [Fact]
+        public void Custom_animals()
+        {
+            var song = Program.GetSong("animal1", "animal2", "animal3", "animal4", "animal5", "animal6", "animal7");
+
+            Assert.Equal(song, @"There was an old lady who swallowed a animal1.
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal2;
+That wriggled and wiggled and tickled inside her.
+She swallowed the animal2 to catch the animal1;
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal3;
+How absurd to swallow a animal3.
+She swallowed the animal3 to catch the animal2,
+She swallowed the animal2 to catch the animal1;
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal4;
+Fancy that to swallow a animal4!
+She swallowed the animal4 to catch the animal3,
+She swallowed the animal3 to catch the animal2,
+She swallowed the animal2 to catch the animal1;
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal5;
+What a hog, to swallow a animal5!
+She swallowed the animal5 to catch the animal4,
+She swallowed the animal4 to catch the animal3,
+She swallowed the animal3 to catch the animal2,
+She swallowed the animal2 to catch the animal1;
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal6;
+I don't know how she swallowed a animal6!
+She swallowed the animal6 to catch the animal5,
+She swallowed the animal5 to catch the animal4,
+She swallowed the animal4 to catch the animal3,
+She swallowed the animal3 to catch the animal2,
+She swallowed the animal2 to catch the animal1;
+I don't know why she swallowed a animal1 - perhaps she'll die!
+
+There was an old lady who swallowed a animal7...
+...She's dead, of course!");
+        }
+
+        [Fact]
         public void Song()
         {
             var song = Program.GetSong();
