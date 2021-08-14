83501b9ad0dd4d5b53b6cd35cfdd26637972dffb
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-14 22:13:52 +0500

    extract method 'GetFirstVerseRefrain'

--- csharp/dotnetcore/song/Program.cs
+++ csharp/dotnetcore/song/Program.cs
@@ -14,25 +14,25 @@ namespace song
         public static string GetSong()
         {
             return @$"{SwallowAnAnimal("fly")}.
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("spider")};
 That wriggled and wiggled and tickled inside her.
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("bird")};
 How absurd to swallow a bird.
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("cat")};
 Fancy that to swallow a cat!
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("dog")};
 What a hog, to swallow a dog!
@@ -40,7 +40,7 @@ She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("cow")};
 I don't know how she swallowed a cow!
@@ -49,7 +49,7 @@ She swallowed the dog to catch the cat,
 She swallowed the cat to catch the bird,
 She swallowed the bird to catch the spider,
 She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
+{GetFirstVerseRefrain()}
 
 {SwallowAnAnimal("horse..")}.
 ...She's dead, of course!";
@@ -58,6 +58,11 @@ I don't know why she swallowed a fly - perhaps she'll die!
         private static string SwallowAnAnimal(string animal)
         {
             return $"There was an old lady who swallowed a {animal}";
+        }
+
+        private static string GetFirstVerseRefrain()
+        {
+            return "I don't know why she swallowed a fly - perhaps she'll die!";
         }
     }
 }
